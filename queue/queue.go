6584d532ae6eecb89b6a98e134025df51085c2d7
Michael Peters <michaelpeterswa@icloud.com>
2022-06-13 12:12:44 -0700

    fix: added queue

--- queue/queue.go
+++ queue/queue.go
@@ -12,9 +12,29 @@ func NewQueue() *Queue {
 	return &Queue{}
 }
 
+func (q *Queue) Len() int {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	return len(q.queue)
+}
+
 func (q *Queue) Push(a any) {
 	q.mu.Lock()
 	defer q.mu.Unlock()
 
 	q.queue = append(q.queue, a)
+}
+
+func (q *Queue) Pop() any {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	if len(q.queue) == 0 {
+		return nil
+	}
+
+	tmp := q.queue[0]
+	q.queue = q.queue[1:]
+	return tmp
 }
--- queue/queue_test.go
+++ queue/queue_test.go
@@ -1,1 +1,50 @@
 package queue_test
+
+import (
+	"testing"
+
+	"676f.dev/utilities/queue"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQueuePushAndPop(t *testing.T) {
+	type test struct {
+		inputs []string
+		want   any
+	}
+
+	tests := []test{
+		{inputs: []string{}, want: nil},
+		{inputs: []string{"a"}, want: "a"},
+		{inputs: []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}, want: "a"},
+	}
+
+	for _, tc := range tests {
+		q := queue.NewQueue()
+		for _, v := range tc.inputs {
+			q.Push(v)
+		}
+		assert.Equal(t, tc.want, q.Pop())
+	}
+}
+
+func TestQueueLen(t *testing.T) {
+	type test struct {
+		inputs []string
+		want   int
+	}
+
+	tests := []test{
+		{inputs: []string{}, want: 0},
+		{inputs: []string{"a"}, want: 1},
+		{inputs: []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}, want: 10},
+	}
+
+	for _, tc := range tests {
+		q := queue.NewQueue()
+		for _, v := range tc.inputs {
+			q.Push(v)
+		}
+		assert.Equal(t, tc.want, q.Len())
+	}
+}
